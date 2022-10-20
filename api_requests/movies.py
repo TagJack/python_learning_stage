87586b001c8337f13a86eb0741faf3c9d543d8af
jack <bbty.jeyhun@gmail.com>
2022-10-20 10:55:30 +0500

    'updates'

--- api_requests/movies.py
+++ api_requests/movies.py
@@ -3,29 +3,45 @@ from bs4 import BeautifulSoup as bs
 
 url = 'https://kinogo-net.la/'
 
-# Loading the page.
-r = requests.get(url)
-r.raise_for_status()
+# # Loading the page.
+# r = requests.get(url)
+# r.raise_for_status()
+#
+# # Creating folder for images.
+# os.makedirs('movies_img', exist_ok=True)
+# # Getting the data
+# soup = bs(r.text, 'html.parser')
+# data = soup.find_all('a', class_='carou img-box')
+#
+# list = []
+# for item in data:
+#     # getting url of the image.
+#     img_url = 'https://kinogo-net.la' + item.img['data-src']
+#     # Loading the image.
+#     img = requests.get(img_url)
+#     file = open(os.path.join('movies_img', os.path.basename(img_url)), 'wb')
+#     for chunk in img.iter_content(100000):
+#         file.write(chunk)
+#     file.close()
+#     # Printing stuff
+#     print(img_url)
+#     print(item['title'])
+#     print(item.getText().strip())
+#     print(item['href'])
+#     print("*"*9+"\n")
 
-# Creating folder for images.
-os.makedirs('movies_img', exist_ok=True)
-# Getting the data
-soup = bs(r.text, 'html.parser')
-data = soup.find_all('a', class_='carou img-box')
+#
+# updates.
+res = requests.get(url)
+res.raise_for_status()
 
-list = []
+soup = bs(res.text, 'html.parser')
+data = soup.select('.msupdate_block_list_item_inner')
+print(len(data))
 for item in data:
-    # getting url of the image.
-    img_url = 'https://kinogo-net.la' + item.img['data-src']
-    # Loading the image.
-    img = requests.get(img_url)
-    file = open(os.path.join('movies_img', os.path.basename(img_url)), 'wb')
-    for chunk in img.iter_content(100000):
-        file.write(chunk)
-    file.close()
-    # Printing stuff
-    print(img_url)
-    print(item['title'])
-    print(item.getText().strip())
-    print(item['href'])
-    print("*"*9+"\n")
+    # print(item.a['href'])
+    # print(item.img['alt'])
+    print(str(item.div()))
+    print('*'*10)
+
+
