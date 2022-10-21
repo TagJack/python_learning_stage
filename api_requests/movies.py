172fa208ae671b6f6fe5abcb47858d9e73e8e82e
jack <bbty.jeyhun@gmail.com>
2022-10-21 11:54:53 +0500

    'updates'

--- api_requests/movies.py
+++ api_requests/movies.py
@@ -36,12 +36,14 @@ res = requests.get(url)
 res.raise_for_status()
 
 soup = bs(res.text, 'html.parser')
-data = soup.select('.msupdate_block_list_item_inner')
-print(len(data))
+data = soup.select('.msupdate_block_list_item')
+
 for item in data:
-    # print(item.a['href'])
-    # print(item.img['alt'])
-    print(str(item.div()))
+    season = ''.join(re.findall(r'\d{,2}\sсезон', str(item))) # Достал сезон
+    print('\n' + item.img.get('alt').strip()) # Название
+    print(season) # Сезон
+    print(item.span.getText()) # Серия и озвучки.
+    print('https://kinogo-net.la' + item.a['href']) # Ссылка.
     print('*'*10)
 
 
