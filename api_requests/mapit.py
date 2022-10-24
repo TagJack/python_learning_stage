0165e5d4d5807f3b0d4eb947a06bdf664191c505
jack <bbty.jeyhun@gmail.com>
2022-10-24 10:15:25 +0500

    'repeating'

--- api_requests/mapit.py
+++ api_requests/mapit.py
@@ -1,11 +1,11 @@
-import requests, sys, webbrowser, pyperclip
-
-url = 'https://www.google.com/maps/place/'
-if len(sys.argv) > 1:
-    # Gets what you write.
-    adress = ' '.join(sys.argv[1:])
-else:
-    # Gets stuff from clickboard.
-    adress = pyperclip.paste()
-
-webbrowser.open(url+adress)
+# import requests, sys, webbrowser, pyperclip
+#
+# url = 'https://www.google.com/maps/place/'
+# if len(sys.argv) > 1:
+#     # Gets what you write.
+#     adress = ' '.join(sys.argv[1:])
+# else:
+#     # Gets stuff from clickboard.
+#     adress = pyperclip.paste()
+#
+# webbrowser.open(url+adress)
